8ce5adc23444ab78214223e6ad78c8139c00170a
Julio Lira <jul10l1r4@gmail.com>
2018-05-08 09:04:42 -0300

    Update req.js

--- req.js
+++ req.js
@@ -1,26 +1,24 @@
-const conex = () => {
-  fetch(
-    `https://atheckos.github.io/api/dicio/algoz.json`
-    ).then((resposta) => {
-      return resposta.text() // Transformando a resposta em texto
-    }).then((text) => {
-        return inf(text)
-    })
-  const inf = (dat) => {
-    if(Response === true){
-      let buff = JSON.parse(dat)
-      let titulo = buff.Title
-      let saap = buff.SAAP
-      let source = buff.Source
-      let descricao = buff.Description
-      let fig = buff.Figurative
-      let plural = buff.Plural
-      let exemplo = buff.Exemple
-      let otros = buff.Other
-      let msg = "sucesso"
-    }else{
-      let msg = "fudeu"
-    }
-    return msg
+fetch(
+  `https://atheckos.github.io/api/dicio/algoz.json`
+  ).then((resposta) => {
+    return resposta.text() // Transformando a resposta em texto
+  }).then((text) => {
+      return inf(JSON.parse(text))
+  })
+const inf = (buff) => {
+  alert(buff.Title)
+  if(buff.Response === "True"){
+    let titulo = buff.Title
+    let saap = buff.SAAP
+    let source = buff.Source
+    let descricao = buff.Description
+    let fig = buff.Figurative
+    let plural = buff.Plural
+    let exemplo = buff.Exemple
+    let otros = buff.Other
+    let msg = "sucesso"
+  }else{
+    let msg = "fudeu"
   }
+  return msg
 }
